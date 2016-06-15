ae7d583f532415b2083bae66466d36841b5f0c90
Seth Tippetts <sethtippetts@gmail.com>
2016-06-14 23:06:21 -0600

    Update production web pack config

--- webpack.production.js
+++ webpack.production.js
@@ -1,29 +1,41 @@
-var Webpack = require('webpack');
+var webpack = require('webpack');
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var buildPath = path.resolve(__dirname, 'dist');
-var mainPath = path.resolve(__dirname, 'app', 'index.js');
+var vendor = require('./package.json').dependencies;
+
+vendor = Object.keys(vendor);
 
 var config = {
 
   // We change to normal source mapping
   devtool: 'source-map',
-  entry: mainPath,
+  entry: {
+    app: './app/index',
+    vendor,
+  },
   output: {
     path: buildPath,
-    filename: 'bundle.js',
+    filename: '[name].js',
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.tpl.html',
       inject: 'body',
     }),
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       loader: 'babel',
       include: path.join(__dirname, 'app'),
+    }, {
+      test: /\.jpe?g$/,
+      loader: 'file-loader',
+    }, {
+      test: /\.svg$/,
+      loader: 'file-loader',
     }, {
       test: /\.css$/,
       loader: 'style!css?modules',
